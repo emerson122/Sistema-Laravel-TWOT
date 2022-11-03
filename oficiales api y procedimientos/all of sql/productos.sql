00814f58ac2fb976f8bd8105686ed099647e85da
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-11-03 02:20:06 -0600

    PRIMER COMMIT

--- oficiales api y procedimientos/all of sql/productos.sql
+++ oficiales api y procedimientos/all of sql/productos.sql
@@ -1,1 +1,8 @@
-CREATE TABLE `productos`.`imagenes` ( `COD_PRODUCTO` BIGINT AUTO_INCREMENT NOT NULL , `Nombre` VARCHAR(145) NOT NULL , `imagen` LONGTEXT NOT NULL , `Precio` BIGINT NOT NULL , PRIMARY KEY (`COD_PRODUCTO`)) ENGINE = InnoDB;
+CREATE TABLE
+    `productos`.`imagenes` (
+        `COD_PRODUCTO` BIGINT AUTO_INCREMENT NOT NULL,
+        `Nombre` VARCHAR(145) NOT NULL,
+        `imagen` LONGTEXT NOT NULL,
+        `Precio` BIGINT NOT NULL,
+        PRIMARY KEY (`COD_PRODUCTO`)
+    ) ENGINE = InnoDB;
